e91cd4a4f3ecbfe521a52b7cc10dcf5adb972edc
ganeshbandewar <84829268+ganeshbandewar@users.noreply.github.com>
2021-10-04 10:13:49 +0530

    Update user.js

--- public/user.js
+++ public/user.js
@@ -1,53 +1,40 @@
 const socket = io()
 let name;
-let textarea = document.querySelector('#textarea')
-let messageArea = document.querySelector('.message_portion')
+let text = document.querySelector('#textarea')
+let portionmessage = document.querySelector('.message_portion')
 do {
     name = prompt('Please enter your name: ')
 } while(!name)
 
-textarea.addEventListener('keyup', (e) => {
+text.addEventListener('keyup',function (e)
+ {
     if(e.key === 'Enter') {
-        sendMessage(e.target.value)
+        reply(e.target.value)
     }
 })
 
-function sendMessage(message) {
+function reply(message) {
     let msg = {
         user: name,
         message: message.trim()
     }
-    // Append 
-    appendMessage(msg, 'outgoing')
-    textarea.value = ''
-    scrollToBottom()
-
-    // Send to server 
-    socket.emit('message', msg)
-
+    insert(msg, 'outgoing')
+    text.value = ''
+    takearrowdown()
+socket.emit('message', msg)
 }
-
-function appendMessage(msg, type) {
+function insert(msg, type) {
     let newDiv = document.createElement('div')
     let classIMO = type
     newDiv.classList.add(classIMO, 'message')
-
-    let csstothis = `
-        <h4>${msg.user}</h4>
-        <p>${msg.message}</p>
-    `
+ let csstothis = ` <h4>${msg.user}</h4><p>${msg.message}</p> `
     newDiv.innerHTML = csstothis
-    messageArea.appendChild(newDiv)
-}
-
-
-// Recieve messages 
-socket.on('message', (msg) => {
-    appendMessage(msg, 'incoming')
-    scrollToBottom()
+    portionmessage.appendChild(newDiv)
+} 
+socket.on('message',function(msg){
+    insert(msg, 'incoming')
+    takearrowdown()
 })
-
-
-function scrollToBottom() {
-    messageArea.scrollTop = messageArea.scrollHeight
+function takearrowdown() {
+    portionmessage.scrollTop = portionmessage.scrollHeight
 }
