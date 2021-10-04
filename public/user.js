36d6208f6993a9fd4eca1c63dc9bc36b291afa9d
ganeshbandewar <84829268+ganeshbandewar@users.noreply.github.com>
2021-10-04 10:45:16 +0530

    first

---
+++ public/user.js
@@ -0,0 +1,40 @@
+const socket = io()
+let name;
+let text = document.querySelector('#textarea')
+let portionmessage = document.querySelector('.message_portion')
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
+
+text.addEventListener('keyup',function (e)
+ {
+    if(e.key === 'Enter') {
+        reply(e.target.value)
+    }
+})
+
+function reply(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    insert(msg, 'outgoing')
+    text.value = ''
+    takearrowdown()
+socket.emit('message', msg)
+}
+function insert(msg, type) {
+    let newDiv = document.createElement('div')
+    let classIMO = type
+    newDiv.classList.add(classIMO, 'message')
+ let csstothis = ` <h4>${msg.user}</h4><p>${msg.message}</p> `
+    newDiv.innerHTML = csstothis
+    portionmessage.appendChild(newDiv)
+} 
+socket.on('message',function(msg){
+    insert(msg, 'incoming')
+    takearrowdown()
+})
+function takearrowdown() {
+    portionmessage.scrollTop = portionmessage.scrollHeight
+}
